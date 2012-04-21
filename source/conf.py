664d772e4beebea18c44de2684639f4fce687723
Randall Degges <rdegges@gmail.com>
2012-04-20 22:01:14 -0700

    Updating the title attributes.

--- source/conf.py
+++ source/conf.py
@@ -104,10 +104,10 @@ html_theme_path = ['_themes']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = 'Deploy Django - Useful Patterns for Deploying Django on Heroku'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-#html_short_title = None
+html_short_title = 'Deploy Django'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
